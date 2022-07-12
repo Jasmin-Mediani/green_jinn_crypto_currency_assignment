b026aef746e249f23d99219fea9d6dfbe5fa9b99
Jasmin Mediani <molihua1992@gmail.com>
2022-07-12 16:05:12 +0200

    edited empty value in GJNumberLabel

--- src/components/GJNumberLabel.js
+++ src/components/GJNumberLabel.js
@@ -3,7 +3,7 @@ import React from "react";
 const GJNumberLabel = ({string, value}) => {
 
     return (
-        <h5 className="card-title">{string}: {value}</h5>
+        <h5 className="card-title">{string}: {value ? value : "-"}</h5>
     )
 }
 
